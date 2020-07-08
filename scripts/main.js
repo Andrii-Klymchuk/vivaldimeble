ed920b26625c869e337172d02b35920f4078c733
Andrii <45493611+Andrii-Klymchuk@users.noreply.github.com>
2020-07-08 15:25:59 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -8,13 +8,4 @@ $(document).ready(function () {
     titlePosition: "over",
     playSpeed: 1000,
   })
-  $("#gallery2 a").append("<span></span>")
-  $("#gallery2 a").hover(
-    function () {
-      $(this).children("span").fadeIn(600)
-    },
-    function () {
-      $(this).children("span").fadeOut(200)
-    },
-  )
 })
